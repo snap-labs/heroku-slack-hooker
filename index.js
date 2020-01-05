bc4a20ab16f2ce71a1b1e2e7e0440c54779df8ae
Hugo Hafner <hello@hugohafner.com>
2020-01-05 21:56:03 +0000

    fixed up

--- index.js
+++ index.js
@@ -1,75 +1,100 @@
-var util = require('util');
-var express = require('express');
-var bodyParser = require('body-parser');
-var request = require('request');
+var util = require("util");
+var express = require("express");
+var bodyParser = require("body-parser");
+var request = require("request");
 
 var app = express();
 
 // config:
-app.set('port', (process.env.PORT || 6000));
+app.set("port", process.env.PORT || 6000);
 
 // middlewares:
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({ extended: true  })); 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // routes
-app.post('/hook/*', function (req, res) {
-    var data = req.body;
-    var slackHook = req.params['0'];
-    console.log('heroku hook body: ', h);
-    console.log('target slack webhook url: ', slackHook);
+app.post("/hook/*", function(req, res) {
+  var data = req.body;
+  var slackHook = req.params["0"];
+  console.log("heroku hook body: ", data);
+  console.log("target slack webhook url: ", slackHook);
 
-    var slack = {
-        attachments: [
-            {
-                fallback: "Required plain-text summary of the attachment.",
-                color: "#36a64f",
-                author_name: data.data.app.name,
-                author_link: data.data.app.name,
-                title: "Heroku " + data.data.resource.toUpperCase(),
-                text: "Optional text that appears within the attachment",
-                fields: [
-                    {
-                        title: "Priority",
-                        value: "High",
-                        short: false
-                    }
-                ],
-                footer: "Heroku Notification",
-                ts: Date.now()
-            }
+  var slack = {
+    attachments: [
+      {
+        fallback: "Required plain-text summary of the attachment.",
+        color: "#36a64f",
+        author_name: data.data.app.name,
+        title:
+          "Heroku Event '" +
+          data.resource[0].toUpperCase() +
+          data.resource.slice(1) +
+          "'",
+        title_link: "https://dashboard.heroku.com/apps/" + data.data.app.name,
+        text: "Initiated by " + data.actor.email,
+        fields: [
+          {
+            title: "Event ID",
+            value: data.data.id,
+            short: true
+          }
         ],
-        username: 'heroku-bot',
-        icon_emoji: ':space_invader:'
-    };
+        footer: "Heroku Notification",
+        ts: Date.now()
+      }
+    ],
+    username: "heroku-bot",
+    icon_emoji: ":space_invader:"
+  };
 
-    console.log(util.format('sending to slack webhook: \n%j\n', slack));
+  if (data.data.state) {
+    slack.attachments[0].fields.push({
+      title: "App State",
+      value: data.data.state,
+      short: true
+    });
+  }
+  if (data.data.description) {
+    slack.attachments[0].fields.push({
+      title: "Description",
+      value: data.data.description,
+      short: true
+    });
+  }
+  if (data.data.output_stream_url) {
+    slack.attachments[0].fields.push({
+      title: "Message Stream",
+      value: "<" + data.data.output_stream_url + "|Log URL>",
+      short: true
+    });
+  }
+  console.log(util.format("sending to slack webhook: \n%j\n", slack));
 
-    request.post({
-        url: slackHook,
-        form: {
-            payload: JSON.stringify(slack)
-        }},
-        function(err, httpResponse, body) {
-            console.log('slack response:');
-            if (err) {
-                console.error(err);
-                res.status(400).send('oops');
-                return;
-            }
+  request.post(
+    {
+      url: slackHook,
+      form: {
+        payload: JSON.stringify(slack)
+      }
+    },
+    function(err, httpResponse, body) {
+      console.log("slack response:");
+      if (err) {
+        console.error(err);
+        res.status(400).send("oops");
+        return;
+      }
 
-            console.log(body);
-            res.send('thanks!');
-        }
-    );
-
+      console.log(body);
+      res.send("thanks!");
+    }
+  );
 });
 
 // serve:
-var server = app.listen(app.get('port'), function () {
-    var host = server.address().address;
-    var port = server.address().port;
+var server = app.listen(app.get("port"), function() {
+  var host = server.address().address;
+  var port = server.address().port;
 
-    console.log('listening at http://%s:%s', host, port);
+  console.log("listening at http://%s:%s", host, port);
 });
-
